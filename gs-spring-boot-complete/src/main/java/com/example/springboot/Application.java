2353b04b7f8fe5d4a885a478f1aa6701832df3b7
shote <shote@TEYZ-PC>
2020-01-14 13:53:47 +0100

    Formatage LocalDateTime

--- gs-spring-boot-complete/src/main/java/com/example/springboot/Application.java
+++ gs-spring-boot-complete/src/main/java/com/example/springboot/Application.java
@@ -7,6 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @SpringBootApplication
 public class Application {
@@ -26,8 +30,14 @@ public class Application {
 			for (String beanName : beanNames) {
 				System.out.println(beanName);
 			}
-
 		};
+	}
+	
+	private ISpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.addDialect(new Java8TimeDialect());
+		engine.setTemplateResolver(templateResolver);
+		return engine;
 	}
 
 }
--- gs-spring-boot-complete/src/main/java/com/example/springboot/SimpleController.java
+++ gs-spring-boot-complete/src/main/java/com/example/springboot/SimpleController.java
@@ -1,18 +1,48 @@
 package com.example.springboot;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class SimpleController {
-	@Value("${spring.application.name}")
+public class SimpleController implements ErrorController {
+	//@Value("${spring.application.name}")
 	String appName;
+	private static final String PATH = "/error";
 	
-	@GetMapping("/")
+	@GetMapping("/home") //pour le home
 	public String homePage(Model model) {
 		model.addAttribute("TPs", appName);
 		return "home";
 	}
+	
+	@GetMapping("/date") // à créer pour chaque page: ici pour le "date"
+	public String datePage(Model model) {
+		model.addAttribute("TPs", appName);
+		//model.addAttribute("standardDate", new Date());
+		//model.addAttribute("localDateTime", LocalDateTime.now());
+		//model.addAttribute("localDate", LocalDate.now());
+		//model.addAttribute("timestamp", Instant.now());
+		model.addAttribute("localDateTime", LocalDateTime.now());
+		return "date";
+	}
+
+	@RequestMapping(value = PATH)
+    public String error() {
+        return "Error handling";
+    }
+	
+	@Override
+	public String getErrorPath() {
+		return PATH;
+	}
+	
 }
