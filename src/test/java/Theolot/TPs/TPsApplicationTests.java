51df13d3aa6ff402721916cbe3a8ec54f1072ffc
shote <shote@TEYZ-PC>
2020-01-14 11:03:51 +0100

    MàJ home.html

---
+++ src/test/java/Theolot/TPs/TPsApplicationTests.java
@@ -0,0 +1,13 @@
+package Theolot.TPs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TPsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
